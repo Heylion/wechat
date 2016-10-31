4ff488a6254dd49d84d070083c6ecd5cda43cf30
Heylion <hjs@heylion.top>
2016-10-31 11:44:07 +0800

    commit again

---
+++ src/java/top/heylion/wechat/Demo.java
@@ -0,0 +1,10 @@
+package top.heylion.wechat;
+
+/**
+ * Created by hjs02 on 2016/10/31.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
